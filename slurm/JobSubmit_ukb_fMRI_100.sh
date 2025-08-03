41ceaa58cc317496d1fb97a8cc256c47166312b0
Rogers F. Silva <rsilva@gsu.edu>
2025-08-03 02:33:48 +0000

    added slurm job script for real data, but not updated

---
+++ slurm/JobSubmit_ukb_fMRI_100.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#SBATCH -e /data/users3/fborhan1/MISA-pytorch/slurm_logs/error%A-%a.err
+#SBATCH -o /data/users3/fborhan1/MISA-pytorch/slurm_logs/out%A-%a.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=fborhan1@student.gsu.edu
+#SBATCH --chdir=/data/users3/fborhan1/MISA-pytorch
+#
+#SBATCH -p qTRDGPUH
+#SBATCH --gres=gpu:A100:1
+#SBATCH --account=trends53c17
+#SBATCH --job-name=torchMISA
+#SBATCH --verbose
+#SBATCH --time=7200
+#
+#SBATCH --nodes=1
+#SBATCH --mem=128g
+#SBATCH --cpus-per-task=32
+
+
+sleep 5s
+
+hostname
+source ~/.bashrc
+. ~/init_miniconda3.sh
+conda activate pt2
+
+# seed=(7 14 21)
+# w=('wpca' 'w0' 'w1')
+
+declare -i SEED=7
+echo $SEED
+W="wpca"
+echo $W
+
+declare -i n_dataset=100
+declare -i n_source=12
+declare -i n_sample=32768
+lrs=(0.01)
+batch_size=(316)
+patience=(10)
+gpu=("A100")
+#Adam optimizer parameters
+adam_params=(0) #0 sets foreach and fused to false, 1 sets foreach=true and fused=false, 2 for foreach=false and fused=true
+beta1=(0.7)
+beta2=(0.65)
+
+experimenter="$USER"
+configuration="/data/users3/fborhan1/MISA-pytorch/configs/sim-siva.yaml"
+
+data_file="sim-siva_dataset"$n_dataset"_source"$n_source"_sample"$n_sample"_seed"$SEED".mat"
+declare -i num_experiments=${#lrs[@]}
+# for ((i=0; i<num_experiments; i++)); do
+#     python main.py -c "$configuration" -f "$data_file" -r results/MathPath2024/ -w "$W" -a -lr "${lrs[$i]}" -b1 "${beta1[$i]}" -b2 "${beta2[$i]}" -bs "${batch_size[$i]}" -e "$experimenter" -ff "${adam_params[$i]}" -p "${patience[$i]}" -s "$SEED" -g "${gpu[$i]}" &
+#     sleep 5s
+#     wait 
+# done
+
+python3 main.py -c /data/users4/xli/MISA/MISA-pytorch/configs/sim-iva.yaml -f sim-siva_dataset100_source12_sample32768_seed14.mat -r runs/BHI25/lr0.007_bs200_ab1-0.95_ab2-0.87 -w
+
+# echo parameters, including variable name and value:
+echo ""
+echo "slurm_job_id: $SLURM_JOB_ID"
+echo "experimenter: $experimenter"
+echo "SEED: $SEED"
+echo "n_dataset: $n_dataset"
+echo "n_source: $n_source"
+echo "n_sample: $n_sample"
+echo "data_file: $data_file"
+echo "configuration: $configuration"
+echo "W: $W"
+echo "lrs: ${lrs[@]}"
+echo "batch_size: ${batch_size[@]}"
+echo "patience: ${patience[@]}"
+echo "gpu: ${gpu[@]}"
+echo "adam_params: ${adam_params[@]} #0 sets foreach and fused to false, 1 sets foreach=true and fused=false, 2 for foreach=false and fused=true"
+echo "beta1: ${beta1[@]}"
+echo "beta2: ${beta2[@]}"
+echo "hostname: $(hostname)"
+echo "date: $(date)"
+echo "pwd: $(pwd)"
+echo "slurm_job_name: $SLURM_JOB_NAME"
+echo "slurm_job_cpus_per_node: $SLURM_JOB_CPUS_PER_NODE"
+echo "slurm_job_partition: $SLURM_JOB_PARTITION"
+echo "slurm_job_mem: $SLURM_JOB_MEM"
+
+
+sleep 5s
